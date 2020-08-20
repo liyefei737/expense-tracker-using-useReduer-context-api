fff6cb13ea459df06622fc4713075600dd95ee2e
liyefei737 <macrofly@gmail.com>
2020-08-19 21:17:33 -0700

    init components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./style.css";
 
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpense } from "./components/IncomeExpense";
+import { TransactionList  } from './components/TransactionList'
+import { AddTransaction } from "./components/AddTransaction";
+
 export default function App() {
   return (
     <div>
-    hello
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
